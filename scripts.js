5ddd27fe4cc3fd1a5fe89e422e5a03c02aaeaa6d
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-06 23:24:17 -0600

    Add: (index.html>Hunk 1) : Link external JS Add: (scripts.js>Hunk 1) : Add test statements to test link

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+//Add alert 
+alert("Hello from TreeHouse."); 
+
+//Current webpage and write command
+document.write("<h1>Welcome to JavaScript Basics</h1>");
+
+//Add alert
+alert("Thanks for visiting.");
+
+//Add string to log
+console.log("Program complete");
