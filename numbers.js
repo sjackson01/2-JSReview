9de423df30af62244fd4c28eec23f56e7cc6feb6
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-09 20:33:12 -0600

    Add (numbers.js>Hunk 1) : Covert strings to numbers using parseInt and parseFloat

--- numbers.js
+++ numbers.js
@@ -13,3 +13,17 @@ var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
 
 document.write('There are ' + secondsPerDay + ' per in a day.');
 
+//Convert string values to int values 
+var HTMLBadges = prompt('How many HTML badges do you have?');
+var CSSBadges = prompt('How many CSS badges do you have?');
+var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+alert('Wow! You have ' + totalBadges + ' badges!');
+
+*/
+
+//Convert string values to float
+var string1 = '2.14'
+var string2 = '3.15'
+var total = parseFloat(string1) + parseFloat(string2);
+
+document.write(total);s
