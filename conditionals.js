53ff5a6f41629de38cf05535b01ede411e88c554
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-15 23:43:31 -0600

    Add (conditionals>Hunk 1) : Add basic JS conditional and test Add (index.php>Hunk 1) : Include script source

--- conditionals.js
+++ conditionals.js
@@ -1,2 +1,13 @@
 /* JS Review Numbers */
 /* Comment out code to manipulate */ 
+
+//Quiz Exercise
+ var answer = prompt('What programming language is the name of a gem?');
+
+ //Test if the value is exactly the same 
+ //Convert input string to uppercase 
+ if(answer.toUpperCase === 'RUBY'){
+    document.write("<p>That's right!</p>");
+    }else{
+    document.write('<p>That\'s wrong!'); 
+}
