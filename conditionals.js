c69f4819501c777846742ee6b808a7014c06b24f
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-16 16:27:07 -0600

    Move (conditionals.js>Hunk 1) : Move comment Modify (conditionals.js>Hunk 2) : Revise simple game to use boolean validation and test

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,8 @@
 /* JS Review Numbers */
 /* Comment out code to manipulate */ 
 
+/*
+
 //Quiz Exercise
  var answer = prompt('What programming language is the name of a gem?');
 
@@ -11,14 +13,27 @@
     }else{
     document.write('<p>That\'s wrong!'); 
 }
-*/ 
+
+*/
 
 //Random Number Guessing Game
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6) +1;
 var guess = prompt('I am thinkng of a number between 1 and 6. What is it?');
 //Convert string received from prompt into int
 if(parseInt(guess) === randomNumber){
-document.write('<p>You guessed the number!</p>');
+correctGuess = true; 
+}
+if(correctGuess === true){
+    document.write('<p> you guess the number!</p>');
 }else{
-    document.write('<p>Sorry, the number was ' + randomNumber + '</p>');
+    document.write('<p>Sorry, the number was ' + randomNumber + '.</p>');
+}
+
+//Boolean value review 
+//All conditions return a true and false value
+if(true){
+    document.write('The condition is true');
+}else{
+    document.write('The condition is false');
 }
