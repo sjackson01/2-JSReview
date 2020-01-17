b70e6f666db2c0afd324e39c508a1d2f2caec5f8
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-17 14:33:05 -0600

    Add (conditionals.js>Hunk 1) : Create a simple program to test conditionals in JS and test

--- conditionals.js
+++ conditionals.js
@@ -49,3 +49,59 @@ if(correctGuess === true){
         document.write('<p>Sorry, the number was ' + randomNumber + '.</p>');
     }
 
+*/
+
+//Comparison Operators (both and &&) (one or ||)
+//Quiz Exercise
+
+//Add one to the var for each question answered correctly 
+var correct = 0;
+
+var answer1 = prompt('Name a programming language that\'s also a gem?');
+//Convert answer to upper case 
+//Allows student to put any case answer 
+if(answer1.toUpperCase() === 'RUBY');{
+    correct += 1; 
+}
+
+var answer2 = prompt('Name a programming language that\'s also a snake?');
+//Convert answer to upper case 
+//Allows student to put any case answer 
+if(answer2.toUpperCase() === 'PYTHON');{
+    correct += 1; 
+}
+
+var answer3 = prompt('Name the language this is also used to style webpages?');
+//Convert answer to upper case 
+//Allows student to put any case answer 
+if(answer3.toUpperCase() === 'CSS');{
+    correct += 1; 
+}
+
+var answer4 = prompt('Name what language do you use to give structure to a website?');
+//Convert answer to upper case 
+//Allows student to put any case answer 
+if(answer4.toUpperCase() === 'HTML');{
+    correct += 1; 
+}
+
+var answer4 = prompt('Name what language do you use add interactivity to a webpage?');
+//Convert answer to upper case 
+//Allows student to put any case answer 
+if(answer4.toUpperCase() === 'JavaScript');{
+    correct += 1; 
+}
+
+//Output results
+document.write("You got " + correct + " out of 5 questions correct. </p>");
+
+//Output rank
+if(correct === 5){
+    document.write("<p><strong>You earned a gold crown!</strong></p>");
+}else if(correct >= 3){
+    document.write("<p><strong>You earned a silver crown.</strong></p>");
+}else if (correct >= 1){
+    document.write("<p><strong>You earned a bronze crown.</strong></p>");
+}else{
+    document.write("<p><strong>You didn't earn any prizes</strong></p>");
+}
