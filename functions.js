e34256291835a32d6e81421cb7713cee115187e2
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-19 13:17:53 -0600

    Modify (funcitons.js>Hunk 1) : getRandomNumber() to return random number Add (functions.js>Hunk 1) : isEmailEmpty() to validate form input and display alert if field empty

--- functions.js
+++ functions.js
@@ -1,11 +1,32 @@
 /* JS Review Functions */
 /* Comment out code to manipulate */ 
 
+
 //Create random number function
-function alertRandom(){
+function getRandomNumber(){
     var randomNumber = Math.floor( Math.random() +6) + 1;
-    alert(randomNumber);
+    return randomNumber;
 }    
 
-//Call function 
-alertRandom();;
+alert( getRandomNumber());
+
+//Validate form field
+function isEmailEmpty(){
+    var field = document.getElementById('email');
+    if (field.value === ''){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Display message if field empty 
+var fieldTest = isEmailEmpty();
+if(fieldTest === true){
+    alert('Please provide your email address');
+}
+
+
+
+
+
