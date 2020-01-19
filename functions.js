4e4153ec738bbfa577257634547ab34bb1e8d264
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-18 21:10:59 -0600

    Add (functions.js>Hunk 1) : Create basic JS function an test Add (index.html>Hunk 1) : Link functions.js to index.html

--- functions.js
+++ functions.js
@@ -1,2 +1,11 @@
 /* JS Review Functions */
 /* Comment out code to manipulate */ 
+
+//Create random number function
+function alertRandom(){
+    var randomNumber = Math.floor( Math.random() +6) + 1;
+    alert(randomNumber);
+}    
+
+//Call function 
+alertRandom();;
